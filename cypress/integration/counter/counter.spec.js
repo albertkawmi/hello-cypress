80f9f7b328df0f21fead64c525037decf49a2c98
Albert Kawmi <albert.kawmi@gmail.com>
2019-06-01 14:13:03 +0100

    add @testing-library/cypress

--- cypress/integration/counter/counter.spec.js
+++ cypress/integration/counter/counter.spec.js
@@ -1,17 +1,38 @@
-describe('Counter', () => {
+describe('Counter - using default Cypress syntax', () => {
   beforeEach(() => {
     cy.visit('/');
   });
 
   it('increments', () => {
-    cy.get('#count').should('have.text', '0');
+    cy.get('#result').should('have.text', 'The count is 0');
     cy.get('#increment').click();
-    cy.get('#count').should('have.text', '1');
+    cy.get('#result').should('have.text', 'The count is 1');
   });
 
   it('decrements', () => {
-    cy.get('#count').should('have.text', '0');
+    cy.get('#result').should('have.text', 'The count is 0');
     cy.get('#decrement').click();
-    cy.get('#count').should('have.text', '-1');
+    cy.get('#result').should('have.text', 'The count is -1');
+  });
+});
+
+// See README.md for more info about testing-library
+describe('Counter - using testing-library syntax', () => {
+  const { getByText, queryByText } = cy;
+
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('increments', () => {
+    queryByText('The count is 0').should('exist');
+    getByText('+').click();
+    queryByText('The count is 1').should('exist');
+  });
+
+  it('decrements', () => {
+    queryByText('The count is 0').should('exist');
+    getByText('-').click();
+    queryByText('The count is -1').should('exist');
   });
 });
